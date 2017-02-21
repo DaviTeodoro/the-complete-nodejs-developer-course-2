2df9cda18be7014c9fdf6f515d23223bc92d2e8d
Davi Teodoro <davi.a.teodoro@gmail.com>
2017-02-21 18:21:37 -0300

    Lecture 45

---
+++ hello-express/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//Middlewears
+app.use((req, res, next) => {
+    var now = new Date().toString();
+
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+//hbs helpers
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (t) => t.toUpperCase())
+
+//Routers
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        wellcomeMessage: "Wellcome to the website"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+})
+
+app.listen(3000, () => console.log('Sever is up on port 3000'));
